47eab707acd95a96a08799360675e7c34e0a1976
mxrpheus6 <pkazachenkoo@gmail.com>
2022-09-25 12:39:18 +0300

    release v1.0

---
+++ laba1/task1_var8/task1_var8/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	float a, b, c, square, volume;
+	printf("Enter edge a: ");
+	scanf_s("%f", &a);
+	printf("Enter edge b: ");
+	scanf_s("%f", &b);
+	printf("Enter edge c: ");
+	scanf_s("%f", &c);
+	square = 2 * (a * b + b * c + a * c);
+	volume = a * b * c;
+	printf("Square = %.3f\n", square);
+	printf("Volume = %.3f\n", volume);
+}
