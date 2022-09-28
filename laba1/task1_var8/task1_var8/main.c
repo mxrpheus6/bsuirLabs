a4c71086c05879b860bd210e96624c5ecac485ff
mxrpheus6 <pkazachenkoo@gmail.com>
2022-09-28 21:13:15 +0300

    release v1.0

--- laba1/task1_var8/task1_var8/main.c
+++ laba1/task1_var8/task1_var8/main.c
@@ -1,17 +1,46 @@
 #include <stdio.h>
-
-int main(void) 
+int main(void)
 {
 	printf("LAB 1 TASK 1 by Kazachenko Pavel from GROUP 250504\n\n");
 	float a, b, c, square, volume;
 	printf("Enter edge a: ");
-	scanf_s("%f", &a);
+	if (scanf_s("%f", &a) == 0)
+	{
+		printf("Error. You must write number.\n");
+		return 0;
+	}
+	else if (a <= 0)
+	{
+		printf("Error. Side length always greater than zero.\n");
+		return 0;
+	}
 	printf("Enter edge b: ");
-	scanf_s("%f", &b);
+	if (scanf_s("%f", &b) == 0)
+	{
+		printf("Error. You must write number.\n");
+		return 0;
+	}
+	else if (b <= 0)
+	{
+		printf("Error. Side length always greater than zero.\n");
+		return 0;
+	}
 	printf("Enter edge c: ");
-	scanf_s("%f", &c);
+	if (scanf_s("%f", &c) == 0)
+	{
+		printf("Error. You must write number.\n");
+		return 0;
+	}
+	if (c <= 0)
+	{
+		printf("Error. Side length always greater than zero.\n");
+		return 0;
+	}
 	square = 2 * (a * b + b * c + a * c);
 	volume = a * b * c;
 	printf("Square = %.3f\n", square);
 	printf("Volume = %.3f\n", volume);
+	return 0;
 }
+
+
