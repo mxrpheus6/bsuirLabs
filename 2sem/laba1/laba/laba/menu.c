1e18ac179eb2dd0767f5195401e5d8856775bee4
mxrpheus6 <pkazachenkoo@gmail.com>
2023-02-19 15:27:11 +0300

    release v1.0

--- 2sem/laba1/laba/laba/menu.c
+++ 2sem/laba1/laba/laba/menu.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 
 #include "menu.h"
 
-int mainMenu(void) {
+void mainMenu(void) {
 	printf("Добро пожаловать в первую лабу! Уровень заданий знатно повысился (почему?..)\nВыбирайте!");
 	
 	
